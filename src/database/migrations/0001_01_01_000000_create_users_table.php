a277408a09e1a6aae3235748ce79e116dc5b1d2e
sajjadnasiribrn <sajjad.nasiri.brn@gmail.com>
2026-01-02 10:37:42 +0330

    edit user table

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+
+            $table->string('phone', 20)->unique()->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->string('google_id')->unique()->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
